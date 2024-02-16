71c48b4b97aa4e40ee5ee5ec2380ba92070f9b79
Babatunde Balogun <balogunbabatunde42@gmail.com>
2024-02-16 10:31:15 +0100

    nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
@@ -6,6 +6,7 @@ const NavBar = () => {
     <div className='w-screen flex gap-5 items-center mb-10 mt-5 justify-center'>
         <NavLink>Home</NavLink>
         <NavLink to={"/country"}>Country</NavLink>
+        <NavLink to={"/test"}>Test Api</NavLink>
     </div>
   )
 }
